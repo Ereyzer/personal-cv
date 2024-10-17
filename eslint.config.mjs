42e84614c68b065767531322e1fa7de43b8a5c85
Ivan <ereyzer142@gmail.com>
2024-10-17 15:20:42 +0300

    add eslint and set up pre commit

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import html from "@html-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    // recommended configuration included in the plugin
+    ...html.configs["flat/recommended"],
+    files: ["**/*.html"], 
+    plugins: {
+      prettier,
+    } ,
+    rules: {
+      'prettier/prettier' : 'error' , 
+      'semi' : [ 'warn' , 'always' ], 
+    },
+  },
+  pluginJs.configs.recommended,
+];
