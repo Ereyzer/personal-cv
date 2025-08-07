fac0ad11508d134e84f929f6aeda8ba6d8e0fa43
Ivan <ereyzer142@gmail.com>
2025-08-07 21:15:39 +0300

    test links

--- src/js/components/softSkills.js
+++ src/js/components/softSkills.js
@@ -1,8 +1,9 @@
 import { softSkillsListRef } from '../config/references';
 import apiService from '../services/api-service';
 
-const image = new URL('../../images/svg/Frame.svg', import.meta.url);
+// const image = new URL('../../images/svg/Frame.svg', import.meta.url);
 
+//   <img src="${image}" alt="defoult skill logo" />
 export async function getSoftSkills (language) {
   const softSkills = await apiService.getSoftSkills(language.toUpperCase());
 
@@ -12,7 +13,6 @@ export async function getSoftSkills (language) {
     li.setAttribute('offer-id', _id);
     li.innerHTML = `<div class="skill-card">
     <div class="skill-icon">
-      <img src="${image}" alt="defoult skill logo" />
     </div>
     <h4 class="skill-title">${title}</h4>
     <p class="skill-text">
