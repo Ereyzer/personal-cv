9560e91ab00272304076b301c78f9aa9663cdc0d
Ivan <ereyzer142@gmail.com>
2025-08-08 18:08:58 +0300

    optimize work with dom

--- src/js/config/references.js
+++ src/js/config/references.js
@@ -5,10 +5,11 @@ export const avatarRef = document.getElementById('home-avatar-img');
 export const introRef = document.getElementById('home-intro-text');
 export const aboutRef = document.getElementById('about-text');
 
-export const linkedinRef = document.getElementById('social-link-linkedin');
-export const githubRef = document.getElementById('social-link-github');
-export const instagramRef = document.getElementById('social-link-instagram');
-export const facebookRef = document.getElementById('social-link-facebook');
-export const telegramRef = document.getElementById('social-link-telegram');
+export const socialLinkListRef = document.getElementById('social-link-list');
+// export const linkedinRef = document.getElementById('social-link-linkedin');
+// export const githubRef = document.getElementById('social-link-github');
+// export const instagramRef = document.getElementById('social-link-instagram');
+// export const facebookRef = document.getElementById('social-link-facebook');
+// export const telegramRef = document.getElementById('social-link-telegram');
 
 export const softSkillsListRef = document.getElementById('soft-skills-list');
