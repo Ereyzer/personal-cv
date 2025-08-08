8ab1c2d779b9a703fd81b88172e2fe7d345cb0c1
Ivan <ereyzer142@gmail.com>
2025-08-08 08:47:00 +0300

    off all api

--- src/js/config/references.js
+++ src/js/config/references.js
@@ -1,14 +1,14 @@
-export const location = window.location;
+// export const location = window.location;
 export const language = document.children[0].getAttribute('lang');
 
-export const avatarRef = document.getElementById('home-avatar-img');
-export const introRef = document.getElementById('home-intro-text');
-export const aboutRef = document.getElementById('about-text');
+// export const avatarRef = document.getElementById('home-avatar-img');
+// export const introRef = document.getElementById('home-intro-text');
+// export const aboutRef = document.getElementById('about-text');
 
-export const linkedinRef = document.getElementById('social-link-linkedin');
-export const githubRef = document.getElementById('social-link-github');
-export const instagramRef = document.getElementById('social-link-instagram');
-export const facebookRef = document.getElementById('social-link-facebook');
-export const telegramRef = document.getElementById('social-link-telegram');
+// export const linkedinRef = document.getElementById('social-link-linkedin');
+// export const githubRef = document.getElementById('social-link-github');
+// export const instagramRef = document.getElementById('social-link-instagram');
+// export const facebookRef = document.getElementById('social-link-facebook');
+// export const telegramRef = document.getElementById('social-link-telegram');
 
-export const softSkillsListRef = document.getElementById('soft-skills-list');
+// export const softSkillsListRef = document.getElementById('soft-skills-list');
