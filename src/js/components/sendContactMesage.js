44bee706696abdfea3ba107f44fe535a6503bbca
Ivan <ereyzer142@gmail.com>
2025-08-13 14:14:49 +0300

    fix not email error

--- src/js/components/sendContactMesage.js
+++ src/js/components/sendContactMesage.js
@@ -2,16 +2,24 @@ import apiService from '../services/api-service';
 
 export const createSender = async () => {
   const contactModalFormRef = document.getElementById('contact-modal-form');
+  const closeBtn = document.querySelector('[modal-contact-button]');
 
   contactModalFormRef.addEventListener('submit', e => {
     e.preventDefault();
     // TODO: cheeck if email
     const { name, email, message } = e.target.elements;
+    if (valid(email)) return;
 
     apiService.sendEmail({
       name: name.value,
       email: email.value,
       text: message.value
     });
+    closeBtn.click();
   });
 };
+
+function valid (email) {
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return pattern.test(email);
+}
